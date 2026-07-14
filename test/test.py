70c4e10c321d759692d1715a4ac93506cef96bca
Sebastien Riou <sebastien.riou@pqshield.com>
2026-07-14 13:50:49 +0200

    remove old tests

--- test/test.py
+++ test/test.py
@@ -1,10 +1,8 @@
 def main():
     from test.common import parse_test_args
-    from test import test_basic, test_auto_split, test_max_val
+    from test import test_basic
     parse_test_args()
     test_basic.test_it()
-    test_auto_split.test_it()
-    test_max_val.test_it()
 
     
 if __name__ == '__main__':
