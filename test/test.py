8cc6ebb129eff1072bddffddd2641aa6fa1c246d
Sebastien Riou <sebastien.riou@pqshield.com>
2026-07-14 14:10:44 +0200

    lint

--- test/test.py
+++ test/test.py
@@ -1,12 +1,10 @@
 def main():
     from test.common import parse_test_args
     from test import test_basic
+
     parse_test_args()
     test_basic.test_it()
 
-    
+
 if __name__ == '__main__':
     main()
-
-
-    
--- test/test_basic.py
+++ test/test_basic.py
@@ -1,29 +1,27 @@
 from test.common import *
 from vaser import Vaser
 
+
 def test_it():
-    args = [4,12]
+    args = [4, 13]
     s = Vaser(args)
     s.finalize()
-    check_test_case(args,s)
+    check_test_case(args, s)
 
-    MAX_VAL = (1<<64)-1 #not really a max of the implementation, more a max of what we test
-    args = [4,MAX_VAL,MAX_VAL,MAX_VAL, 255,1<<17,2,8,1<<32,128,1<<32]
-    for i in range(1,MAX_VAL.bit_length()):
+    MAX_VAL = (1 << 64) - 1  # not really a max of the implementation, more a max of what we test
+    args = [4, MAX_VAL, MAX_VAL, MAX_VAL, 255, 1 << 17, 2, 8, 1 << 32, 128, 1 << 32]
+    for i in range(1, MAX_VAL.bit_length()):
         first = 1 << i
         all_args = [first] + args
         s = Vaser(all_args)
         s.finalize()
-        check_test_case(all_args,s)
+        check_test_case(all_args, s)
     all_args = [MAX_VAL] + args
     s = Vaser(all_args)
     s.finalize()
-    check_test_case(all_args,s)
-    
-   
+    check_test_case(all_args, s)
+
+
 if __name__ == '__main__':
     parse_test_args()
     test_it()
-
-
-    
